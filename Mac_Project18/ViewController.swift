46b829b093c374c34b65d99d910ee5dac5c948e6
Glenn Cole <gcole@mac.com>
2018-10-28 17:37:39 -0700

    Initial Commit

---
+++ Mac_Project18/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Mac_Project18
+//
+//  Created by Glenn Cole on 10/28/18.
+//  Copyright © 2018 Glenn Cole. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
